42354284d370a1ef70c32be7bd79fb9424441c77
Athar Mahboob <athar.cse1@gmail.com>
2025-09-04 13:03:02 +0530

    bug fix

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "https://www.wmshostings.us",
+      "https://wmshostings.us",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
