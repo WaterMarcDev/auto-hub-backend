1869e2e3698dac15095088d8b502f017dca111df
Athar Mahboob <athar.cse1@gmail.com>
2025-10-01 08:35:29 +0530

    cookie config

--- controllers/authController.js
+++ controllers/authController.js
@@ -97,7 +97,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
