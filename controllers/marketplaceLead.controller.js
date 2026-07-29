2fe72fe657e3d874ba45d1cc96fb0b4cc67259e2
Tomar1234 <tomarshiva7017@gmail.com>
2026-07-30 01:45:17 +0530

    ebay_Get_Messages_fix

--- controllers/marketplaceLead.controller.js
+++ controllers/marketplaceLead.controller.js
@@ -401,36 +401,44 @@ exports.syncAll = async (req, res) => {
 };
 
 //Ebay conversations function
+// exports.testEbayConversations = async (req, res) => {
+//   try {
+//     const IntegrationAccount = require("../models/IntegrationAccount.model");
+//     const ebayAdapter = require("../services/adapters/ebayAdapter");
+
+//     const account = await IntegrationAccount.findOne({
+//       platform: "ebay",
+//       isConnected: true,
+//     });
+
+//     if (!account) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "No connected eBay account found.",
+//       });
+//     }
+
+//     const result = await ebayAdapter.testConversations(account);
+
+//     return res.status(200).json({
+//       success: true,
+//       data: result,
+//     });
+//   } catch (err) {
+//     console.error(err);
+
+//     return res.status(500).json({
+//       success: false,
+//       message: err.message,
+//       response: err.response?.data || null,
+//     });
+//   }
+// };
 exports.testEbayConversations = async (req, res) => {
-  try {
-    const IntegrationAccount = require("../models/IntegrationAccount.model");
-    const ebayAdapter = require("../services/adapters/ebayAdapter");
+    console.log("===== TEST CONTROLLER HIT =====");
 
-    const account = await IntegrationAccount.findOne({
-      platform: "ebay",
-      isConnected: true,
+    return res.json({
+        success: true,
+        message: "Controller reached"
     });
-
-    if (!account) {
-      return res.status(404).json({
-        success: false,
-        message: "No connected eBay account found.",
-      });
-    }
-
-    const result = await ebayAdapter.testConversations(account);
-
-    return res.status(200).json({
-      success: true,
-      data: result,
-    });
-  } catch (err) {
-    console.error(err);
-
-    return res.status(500).json({
-      success: false,
-      message: err.message,
-      response: err.response?.data || null,
-    });
-  }
 };
