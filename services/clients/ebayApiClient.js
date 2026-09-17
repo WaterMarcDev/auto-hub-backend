c1ac273baec2cf60ac899fdd7e618fae54a27a89
Tomar1234 <tomarshiva7017@gmail.com>
2026-09-18 01:11:56 +0530

    ebay_part_syncing_fix

--- services/clients/ebayApiClient.js
+++ services/clients/ebayApiClient.js
@@ -338,6 +338,14 @@ class EbayApiClient {
    */
   async post(accessToken, endpoint, data = {}, params = {}) {
     const client = this._createClient(accessToken);
+
+    console.log("[EBAY_HTTP_DEBUG] POST", {
+      endpoint,
+      headers: client.defaults?.headers?.toJSON
+        ? client.defaults.headers.toJSON()
+        : client.defaults?.headers,
+    });
+    
     return this._executeWithRetry(() => client.post(endpoint, data, { params }));
   }
 
